257782790ada9ab3910ffbf20da477b33aa44ba9
Andrew Moss <andyjosephmoss@gmail.com>
2023-07-17 14:33:25 +0200

    Query and Path params

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::SocketAddr;
 
+use axum::extract::Path;
 use axum::extract::Query;
 use axum::response::{Html, IntoResponse};
 use axum::routing::get;
@@ -8,7 +9,9 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello-world", get(handler_hello));
+    let routes_hello = Router::new()
+        .route("/hello-world", get(handler_hello_query))
+        .route("/hello-world/:name", get(handler_hello_path));
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
@@ -22,10 +25,23 @@ struct HelloParams {
     name: Option<String>,
 }
 
-async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
-    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+async fn handler_hello_query(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!(
+        "->> {:<12} - handler_hello query param - {params:?}",
+        "HANDLER"
+    );
 
     let name = params.name.as_deref().unwrap_or("World!!");
     let resp = format!("Hello <strong>{name}!!</strong>");
+    Html(resp)
+}
+
+async fn handler_hello_path(Path(name): Path<String>) -> impl IntoResponse {
+    println!(
+        "->> {:<12} - handler_hello path variable - {name}",
+        "HANDLER"
+    );
+
+    let resp = format!("Hello path param <strong>{name}</strong>");
     Html(resp)
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -6,6 +6,7 @@ async fn quick_dev() -> Result<()> {
 
     hc.do_get("/hello-world").await?.print().await?;
     hc.do_get("/hello-world?name=Andrew").await?.print().await?;
+    hc.do_get("/hello-world/Andrew").await?.print().await?;
 
     Ok(())
 }
